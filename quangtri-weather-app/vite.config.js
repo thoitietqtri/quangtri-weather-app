b52b86943cb4a6ddccf200dc2c7758f8cce41f24
thoitietqtri <ngoduong28093@gmail.com>
2026-06-03 09:27:33 +0700

    Update vite.config.js

--- quangtri-weather-app/vite.config.js
+++ quangtri-weather-app/vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'index.html'
-    }
-  }
+  base: '/quangtri-weather-app/'
 })
